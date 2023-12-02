5bf8c62d2bf4c25748160cab3b9c65d20c35d216
74679aishwarya <127942967+74679aishwarya@users.noreply.github.com>
2023-12-02 22:04:18 +0530

    Add files via upload

---
+++ rev.py
@@ -0,0 +1,162 @@
+# print("hey i am aish \n and this viewer is also good")
+
+
+# print(" hey i am a good \"good\" girl ")
+
+
+# print("hey", 6,7 ,sep=" ")
+
+
+# print("hey", 6,7 ,sep="@", end="009")
+# print("aish")
+
+
+
+# a=complex(8,2)
+# print(type(a))
+
+
+
+# a=15
+# b=6
+# c=a//b                  #floor division
+# print(c)
+
+
+
+# print(5**4)
+
+
+
+# a="aish"
+# b="warya"
+# print(a+b)                 #string concanating
+
+
+
+# a="79"
+# b="90"
+# print(int(a)+int(b))
+
+
+# a="aish"
+# b="9"
+# print(int(a)+int(b))                 #error
+
+
+# x=int(input("enter 1st no"))
+# y=int(input("enter 2nd no"))
+# print(x+y)
+
+
+
+# name="aishwarya"
+# print("hello"+ " " +name)
+
+
+# string="aishwarya"
+# print(string[6])
+
+# name="aishwarya, pooja"
+# print(name[0:9])
+# print(len(name))
+
+# fruit="mango"
+# print(fruit[:5])
+# print(fruit[0:])
+# print(fruit[2:5])
+# print(fruit[-1:-3])
+# print(fruit[-3:-1])
+
+
+
+# alphabets="abcde"
+# for i in alphabets:
+#     print(i)
+
+
+
+# a="aishwarya is Good"
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+
+
+# string = "welcome to the console!!!"
+# print(string.center(50))
+# print(len(string.center(50)))
+# print(string.endswith("!!!"))
+
+
+# str="aish is good , aish is clumsy, aish is my name"
+# print(str.count("aish"))
+# print(str.endswith("od",2,12))
+# print(str.find("good"))
+# print(str.find("pooja"))                          #-1 if index will not found
+# print(str.startswith("aish"))
+
+
+
+# str="welcome to the console"
+# print(str.isalnum())
+# print(str.isalpha())
+# print(str.islower())
+
+
+# str="we wish u a very happy birthday\n"
+# print(str.isprintable())                                #becoz of \n
+
+
+# str="   "
+# print(str.isspace())                                #true
+
+
+# str="World Health Organization"
+# str1="World health organization"
+# print(str.istitle())                               #true
+# print(str1.istitle())                              #false
+
+
+# str="python is a prog language"
+# print(str.swapcase())
+
+# a="aish!!!!"
+# print(a.rstrip("!"))
+# a="!!!aish"
+# print(a.rstrip("!"))
+# print(a.replace("aish","pooja"))
+
+# a="!!!aish!!!is a good girl "
+# print(a.split(" "))
+
+
+# heading="intro to Python"
+# print(heading.capitalize())
+
+
+# for i in [1,0]:
+#     print(i+1)
+
+# l1=[1,2,3,[4]]
+# l2=list(l1)
+# print(id(l1)==id(l2))
+
+# x=['ab','cd'];
+# print(len(list(map(list,x))))
+
+# l=[-2,4]
+# m=map(lambda x:x*2,l)
+# print(m)
+
+
+# x=[12,34]
+# print(len(''.join(list(map(str,x)))))
+
+# a=12
+# print(type(a))
+
+# print("xyyzxyzxzxyy".count('xyy',2,11))
+
+test={1:'A',2:'B',3:'c'}
+test={}
+print(len(test))
